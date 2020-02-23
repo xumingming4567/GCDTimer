7f55b6697c196c97a876a99b0a21a5e6423faa38
xumingming4567 <xumingming6457@163.com>
2020-02-23 19:32:40 +0800

    添加计时器

---
+++ GCDTimer/CustomGCDTimer.h
@@ -0,0 +1,17 @@
+//
+//  CustomGCDTimer.h
+//  GCDTimer
+//
+//  Created by xmm on 2020/2/23.
+//  Copyright © 2020 xmm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomGCDTimer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GCDTimer/CustomGCDTimer.m
@@ -0,0 +1,13 @@
+//
+//  CustomGCDTimer.m
+//  GCDTimer
+//
+//  Created by xmm on 2020/2/23.
+//  Copyright © 2020 xmm. All rights reserved.
+//
+
+#import "CustomGCDTimer.h"
+
+@implementation CustomGCDTimer
+
+@end
